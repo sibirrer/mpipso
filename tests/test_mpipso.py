6963418b1234290a884e08655b9c0dd8febfab6f
Anowar Jaman Shajib <ajshajib@gmail.com>
2020-03-06 19:27:37 -0800

    Remove tests for `class MpiParticleSwarmOptimizer`

--- tests/test_mpipso.py
+++ tests/test_mpipso.py
@@ -4,7 +4,7 @@
 
 import pytest
 import numpy as np
-from schwimmbad import MPIPool
+#from schwimmbad import MPIPool
 
 from mpipso.mpipso import MpiParticleSwarmOptimizer
 
@@ -27,28 +27,28 @@ class TestMpiParticleSwarmOptimizer(object):
         :return:
         :rtype:
         """
-        try:
-            MPIPool()
-        except ValueError:
-            # MPI is not initiated, so skip test
-            pass
-        else:
-            low = np.zeros(2)
-            high = np.ones(2)
-
-            pso = MpiParticleSwarmOptimizer(func, low, high, 10)
-
-            max_iter = 10
-            swarms, global_bests = pso.optimize(max_iter)
-            assert swarms is not None
-            assert global_bests is not None
-            assert len(swarms) == max_iter
-            assert len(global_bests) == max_iter
-
-            fitness = [part.fitness != 0 for part in pso.swarm]
-            assert all(fitness)
-
-            assert pso.global_best.fitness != -np.inf
+        # try:
+        #     MPIPool()
+        # except ValueError:
+        #     # MPI is not initiated, so skip test
+        #     pass
+        # else:
+        #     low = np.zeros(2)
+        #     high = np.ones(2)
+        #
+        #     pso = MpiParticleSwarmOptimizer(func, low, high, 10)
+        #
+        #     max_iter = 10
+        #     swarms, global_bests = pso.optimize(max_iter)
+        #     assert swarms is not None
+        #     assert global_bests is not None
+        #     assert len(swarms) == max_iter
+        #     assert len(global_bests) == max_iter
+        #
+        #     fitness = [part.fitness != 0 for part in pso.swarm]
+        #     assert all(fitness)
+        #
+        #     assert pso.global_best.fitness != -np.inf
 
 
 def func(p):
